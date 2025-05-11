c04c5d2419b944dd165acaf42468019ea664f0a3
Matthew Williams <orangegrove_1955@hotmail.com>
2025-05-11 14:28:09 +1000

    Configured setup for CI testing

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/test/setup.ts",
+    include: ["./src/**/*.test.ts[x]"],
   },
   resolve: {
     alias: {
